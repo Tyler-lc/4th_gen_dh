3f8e9fe89be2497fabc8a893e2caed6a18d2adca
Tyler-lc <lucasamassima@gmail.com>
2024-05-28 15:22:54 +0200

    added some debugging options and improved speed further

--- Building/Building.py
+++ Building/Building.py
@@ -26,7 +26,8 @@ class Building:
         self.summer_start = summer_start
         self.summer_end = summer_end
 
-        # Ensure the outside_temperature has a DatetimeIndex
+        # Ensure the outside_temperature has a DatetimeIndex and is a Dataframe
+        # if not a dataframe then create one with DateTimeIndex
         if not isinstance(outside_temperature, pd.DataFrame):
             start = f"{year_start}-01-01"
             period = len(outside_temperature)
@@ -34,16 +35,21 @@ class Building:
             self.outside_temperature = pd.DataFrame(
                 outside_temperature, index=weather_index, columns=["Outside T °C"]
             )
+        # if it is a dataframe but does not have a DatetimeIndex then set it
         elif not isinstance(outside_temperature.index, pd.DatetimeIndex):
             start = f"{year_start}-01-01"
             period = len(outside_temperature)
             weather_index = pd.date_range(start=start, periods=period, freq="1H")
             outside_temperature.set_index(weather_index, inplace=True)
             self.outside_temperature = outside_temperature
+        # if it is a dataframe and has a DatetimeIndex then just use input data
         else:
             self.outside_temperature = outside_temperature
+            weather_index = outside_temperature.index
 
-        self.list_temperature = self.outside_temperature.iloc[:, 0].values.tolist()
+        self.list_temperature = self.outside_temperature.iloc[
+            :, 0
+        ].values.tolist()  # not used for now?
         self.irradiation_data = pd.DataFrame(index=weather_index)
         self.irradiation_data = irradiation_data
 
@@ -97,7 +103,7 @@ class Building:
         self.hourly_useful_demand = pd.DataFrame(
             index=weather_index, columns=["net useful hourly demand [kWh]"]
         )
-        self.heating_months = self.is_summer(self.outside_temperature.index)
+        self.summer_months = self.is_summer(self.outside_temperature.index)
 
     # in this first section we parse the data from the components dataframe to create
     # the data structure we use in the calculations
@@ -157,7 +163,7 @@ class Building:
             index=outside_temp.index,
             columns=surfaces_df["surface_name"],
         )
-        losses_df.loc[self.heating_months] = 0
+        losses_df.loc[self.summer_months] = 0
         return losses_df
 
     def transmission_losses_opaque(
@@ -249,7 +255,7 @@ class Building:
             # compute gain for all hours in one vectorized operation
             gains = global_irradiation * window_SHGC * window_area / 1000
             # Set gains to zero during summer months
-            gains[self.heating_months] = 0
+            gains[self.summer_months] = 0
 
             self.solar_gain[row["surface_name"]] = gains
 
@@ -268,7 +274,7 @@ class Building:
         vent_losses[vent_losses < 0] = 0  # set negative values to zero
 
         # Set losses to zero during summer months
-        vent_losses[self.heating_months] = 0
+        vent_losses[self.summer_months] = 0
 
         self.ventilation_losses["ventilation losses [kWh]"] = vent_losses
 
@@ -319,11 +325,14 @@ class Building:
             + self.ventilation_losses.sum(axis=1)
         )
         total_gains = self.solar_gain.sum(axis=1)
-        total_gains[self.outside_temperature.iloc[:, 0] >= 20] = (
-            0  # TODO: i'm not sure this is the correct way to address the
-        )
+        # total_gains[self.outside_temperature.iloc[:, 0] >= 20] = (
+        #     0  # TODO: i'm not sure this is the correct way to address the
+        # )
+
         net_result = total_losses - total_gains
-        net_result[net_result < 0] = 0
+        mask = net_result < 0
+        net_result[mask] = 0
+
         self.hourly_useful_demand["net useful hourly demand [kWh]"] = net_result
 
     def is_summer(self, date_index):
@@ -394,43 +403,55 @@ class Building:
 
 if __name__ == "__main__":
     import os
-    from pyinstrument import Profiler
+    import sys
+    import timeit
+    from Databases.mysql_utils.mysql_utils import create_connection, fetch_data
 
+    # import the weather and irradiation data
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     path_weather = "../Irradiation_Data/east.csv"
     weather = pd.read_csv(path_weather, usecols=["T2m"])
     irradiation_path = "../Irradiation_Data/irradiation_data.csv"
     irradiation = pd.read_csv(irradiation_path)
 
-    # print(filtered_output)
-    import sys
-    import os
-    import timeit
-
     # Ensure the parent directory is in the Python path
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+    # on/off toggle some tests and debugging options
+    losses_outputs = 0
+    plot = 0
+    test_speed = 1
+    profiler = 0
+
     from Databases.mysql_utils.mysql_utils import create_connection, fetch_data
 
-    profiler = Profiler()
     # test building results
     data = fetch_data(1)
-    profiler.start()
-    sfh = Building("test", "SFH1", data, weather, irradiation)
-    sfh.thermal_balance()
+
+    if profiler == 1:
+        from pyinstrument import Profiler
+
+        profiler = Profiler()
+        profiler.start()
+        sfh = Building("test", "SFH1", data, weather, irradiation)
+        sfh.thermal_balance()
+        profiler.stop()
+        output = profiler.output_text(unicode=True, color=True)
+        filtered_output = "\n".join(
+            line for line in output.split("\n") if "pandas" not in line
+        )
+        print(filtered_output)
+
+    if profiler == 0:
+        sfh = Building("test", "SFH1", data, weather, irradiation)
+        sfh.thermal_balance()
+
     total_ued = sfh.get_sum_useful_demand()
-    profiler.stop()
     original_value = 100452.379147
-    output = profiler.output_text(unicode=True, color=True)
-    filtered_output = "\n".join(
-        line for line in output.split("\n") if "pandas" not in line
-    )
-    print(filtered_output)
+
     print(
         f"Total useful energy demand per year = {total_ued} kWh. Should be: {original_value} kWh"
     )
-
-    test_speed = 1
 
     if test_speed == 1:
         # test the speed of the code
@@ -441,3 +462,70 @@ if __name__ == "__main__":
         time = timeit.timeit(lambda: new_losses(), number=n_runs)
 
         print(f"Time for {n_runs} runs: {time} seconds")
+
+    if plot == 1:
+        import matplotlib.pyplot as plt
+
+        # Assuming `df` is your DataFrame and 'Net Useful Hourly Demand [kWh]' is the column to be plotted
+        df = sfh.get_useful_demand()
+
+        # Calculate the 12-hour rolling average
+        rolling_window = 96
+        df["Smoothed"] = (
+            df["net useful hourly demand [kWh]"].rolling(window=rolling_window).mean()
+        )
+
+        # Plot the original data
+        plt.figure(figsize=(10, 6))
+        plt.plot(
+            df.index,
+            df["net useful hourly demand [kWh]"],
+            label="Original Data",
+            alpha=0.5,
+        )
+
+        # Plot the smoothed data
+        plt.plot(df.index, df["Smoothed"], label="12-hour Rolling Average", color="red")
+
+        # Add title and labels
+        plt.title(
+            f"Net Useful Hourly Demand with {rolling_window}-hour Rolling Average"
+        )
+        plt.xlabel("Time")
+        plt.ylabel("Net Useful Hourly Demand [kWh]")
+        plt.legend()
+
+        # Show the plot
+        plt.show()
+
+    if losses_outputs == 1:
+        # results from each category of losses and gains are saved in csv files.
+        test_results = df.to_csv("test_results.csv")
+        solar_gain = sfh.get_solar_gain()
+        ground_losses = sfh.get_ground_losses()
+        opaque_losses = sfh.get_opaque_losses()
+        transparent_losses = sfh.get_transparent_losses()
+        ventilation_losses = sfh.get_ventilation_losses()
+        useful_demand = sfh.get_hourly_useful_demand()
+        total_useful_energy_demand = sfh.get_total_useful_energy_demand()
+
+        from pathlib import Path
+
+        Path("test_results").mkdir(parents=True, exist_ok=True)
+        solar_gain.to_csv("test_results/solar_gain.csv")
+        ground_losses.to_csv("test_results/ground_losses.csv")
+        opaque_losses.to_csv("test_results/opaque_losses.csv")
+        transparent_losses.to_csv("test_results/transparent_losses.csv")
+        ventilation_losses.to_csv("test_results/ventilation_losses.csv")
+        useful_demand.to_csv("test_results/useful_demand.csv")
+        total_useful_energy_demand.to_csv("test_results/total_useful_energy_demand.csv")
+        print("Results saved in test_results folder.")
+
+    # def thermal_balance(self):
+    #     self.transmission_losses_opaque()  # ok
+    #     self.transmission_losses_transparent()  # ok
+    #     self.transmission_losses_ground()  # ok
+    #     self.sol_gain()  # ok
+    #     self.vent_loss()  # ok
+    #     self.useful_demand()  # ok
+    #     self.total_use_energy_demand()  # ok
