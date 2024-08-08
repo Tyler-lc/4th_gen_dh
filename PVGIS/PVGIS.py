c063e1ad4f388a5ced4afc1ec5eb52b44b7e94e6
Tyler-lc <lucasamassima@gmail.com>
2024-08-08 12:28:15 +0200

    updated script to have hyperparameters and correct values for frankfurt griesheim mitte

--- PVGIS/PVGIS.py
+++ PVGIS/PVGIS.py
@@ -1,36 +1,50 @@
 import requests
 import pandas as pd
 import json
+import os
+from pathlib import Path
 
 
 # Set the API endpoint URL
-url = "https://re.jrc.ec.europa.eu/api/seriescalc?"
+url = "https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?"
 # url = "https://re.jrc.ec.europa.eu/api/v5_2/tool_name?param1=value1&param2=value2&..."
 
+# set-up the azimuths
+
 azimuths = {
-    'south': 0,
-    'south_east': -45,
-    'east': -90,
-    'north_east': -135,
-    'north': 180,
-    'north_west': 135,
-    'west': 90,
-    'south_west': 45
+    "south": 0,
+    "south_east": -45,
+    "east": -90,
+    "north_east": -135,
+    "north": 180,
+    "north_west": 135,
+    "west": 90,
+    "south_west": 45,
 }
+
+# set up some parameters that we would like to change
+lat: float = 50.098
+lon: float = 8.600
+start_year: int = 2020
+end_year: int = 2020
+database: str = "PVGIS-SARAH2"
+pv_calc: int = 0  # we do not want to calculate pv production
+angle: int = 90  # we want a vertical facade
+city: str = "Frankfurt-Griesheim_Mitte"
 
 dfs = {}
 for direction, azimuth in azimuths.items():
     # Define the parameters for the API request
     params = {
-        'lat': 48.210,        # latitude
-        'lon': 16.372,      # longitude
-        'raddatabase': 'PVGIS-SARAH',
-        'startyear': 2015,  # we are only using 2015 as test
-        'endyear': 2015,    # hence also here 2015
-        'pvcalculation': 0, # we are not performing PV, we only want radiation
-        'angle': 90,        # we want a vertical facade
-        'aspect': azimuth,  # set the azimuth direction
-        'outputformat': 'json'
+        "lat": lat,  # latitude
+        "lon": lon,  # longitude
+        "raddatabase": database,
+        "startyear": start_year,  # we are only using 2015 as test
+        "endyear": end_year,  # hence also here 2015
+        "pvcalculation": pv_calc,  # we are not performing PV, we only want radiation
+        "angle": angle,  # we want a vertical facade
+        "aspect": azimuth,  # set the azimuth direction
+        "outputformat": "json",
     }
 
     # Send a GET request to the API
@@ -39,14 +53,23 @@ for direction, azimuth in azimuths.items():
     # Parse the response JSON into a Python dictionary
     data = response.json()
 
-    dfs[direction] = pd.DataFrame(data['outputs']["hourly"])
+    dfs[direction] = pd.DataFrame(data["outputs"]["hourly"])
 
-for directions in dfs:
-    directory = "../Irradiation_Data/"+directions+".csv"
-    dfs[str(directions)].to_csv(directory, sep = ",")
+
+# Saving the data and setting up paths
+path = Path(f"../irradiation_data/{city}")
+path.mkdir(parents=True, exist_ok=True)
+
+# First we save each column in a separate CSV file
+for direction in dfs:
+    file_path = path / f"{city}_{direction}_{start_year}_{end_year}.csv"
+    print(f"Saving data to {file_path}")
+    dfs[direction].to_csv(file_path, sep=",")
 
 irradiation_df = pd.DataFrame()
 for direction, df in dfs.items():
-    irradiation_df[direction + ' G(i) [kWh/m2]'] = df['G(i)']
+    irradiation_df[direction + " G(i) [kWh/m2]"] = df["G(i)"]
 
-irradiation_df.to_csv('../Irradiation_Data/irradiation_data.csv', sep=',')
+final_file_path = path / f"{city}_irradiation_data_{start_year}_{end_year}.csv"
+print(f"Saving final data to {final_file_path}")
+irradiation_df.to_csv(final_file_path, sep=",")
