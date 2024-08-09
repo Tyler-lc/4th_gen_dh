de1f164452bbee5b652a4a07ea10fcb8ecb7ac5a
Tyler-lc <lucasamassima@gmail.com>
2024-08-09 12:45:41 +0200

    the code does not work for now. It does not assign building ages correctly.

--- utils/building_utilities.py
+++ utils/building_utilities.py
@@ -39,27 +39,27 @@ def define_building_type(gdf: gpd.GeoDataFrame) -> pd.Series:
         "5110 Grünanlage",
     ]
     # Translated the german types in english
-    translation_dict = {
-        "1650 Baumarkt": "1650 DIY Store",  # trade
-        "1610 Möbelmarkt": "1610 Furniture Store",  # trade
-        "1320 Handel und Dienstl.": "1320 Trade and Services",  # trade
-        "2204 Bürgerhs.,Stadthalle": "2204 Community Center, Town Hall",  # offices
-        "1300 Industrie u. Gewerbe": "1300 Industry and Commerce",  # other non-residential buildings
-        "1670 Lebensmittelmarkt": "1670 Grocery Store",  # trade
-        "1310 Lagerfläche": "1310 Storage Area",  # other non-residential buildings
-        "9990 Freifläche": "9990 Open Space",  # other non-residential buildings
-        "2440 Sonst. religiöse Zwecke": "2440 Other Religious Purposes",  # other non-residential buildings
-        "7000 V+E allg.": "7000 V+E General",  # other non-residential buildings
-        "2210 Kinderbetreuung": "2210 Childcare",  # education
-        "5306 Fußballplatz": "5306 Soccer Field",  # other non-residential buildings
-        "2860 Gesamtschule": "2860 Comprehensive School",  # education
-        "1910 Garten-/Landschaftsb": "1910 Gardening/Landscaping",  # other non-residential buildings
-        "6380 Garage": "6380 Garage",  # other non-residential buildings
-        "2230 Alteneinrichtung": "2230 Elderly Care Facility",  # health
-        "5500 Nutz-/Freizeitgärten": "5500 Utility/Leisure Gardens",  # other non-residential buildings
-        "5360 Kleintierzucht": "5360 Small Animal Breeding",  # other non-residential buildings
-        "5900 Verkehrsgrün": "5900 Traffic Green",  # other non-residential buildings
-        "5110 Grünanlage": "5110 Green Area",  # other non-residential buildings
+    translation_dict = {  # TODO: we need to actually change these values into something we can use with BSO database
+        "1650 Baumarkt": "trade",  # trade
+        "1610 Möbelmarkt": "trade",  # trade
+        "1320 Handel und Dienstl.": "trade",  # trade
+        "2204 Bürgerhs.,Stadthalle": "offices",  # offices
+        "1300 Industrie u. Gewerbe": "other",  # other non-residential buildings
+        "1670 Lebensmittelmarkt": "trade",  # trade
+        "1310 Lagerfläche": "other",  # other non-residential buildings
+        "9990 Freifläche": "other",  # other non-residential buildings
+        "2440 Sonst. religiöse Zwecke": "other",  # other non-residential buildings
+        "7000 V+E allg.": "other",  # other non-residential buildings
+        "2210 Kinderbetreuung": "education",  # education
+        "5306 Fußballplatz": "other",  # other non-residential buildings
+        "2860 Gesamtschule": "education",  # education
+        "1910 Garten-/Landschaftsb": "other",  # other non-residential buildings
+        "6380 Garage": "other",  # other non-residential buildings
+        "2230 Alteneinrichtung": "health",  # health
+        "5500 Nutz-/Freizeitgärten": "other",  # other non-residential buildings
+        "5360 Kleintierzucht": "other",  # other non-residential buildings
+        "5900 Verkehrsgrün": "other",  # other non-residential buildings
+        "5110 Grünanlage": "other",  # other non-residential buildings
     }
 
     # Translate the building_use column
@@ -110,7 +110,9 @@ def define_building_type(gdf: gpd.GeoDataFrame) -> pd.Series:
     return defined_typologies
 
 
-def define_building_age(gdf: gpd.GeoDataFrame, age_distr: dict) -> pd.Series:
+def define_building_age(
+    gdf: gpd.GeoDataFrame, age_distr: pd.DataFrame, residential: bool
+) -> pd.Series:
     """
     this function defines the age of the buildings. The age is randomly assigned to the buildings
     based on the statistics that we have on the building stock. this function takes the
@@ -119,10 +121,61 @@ def define_building_age(gdf: gpd.GeoDataFrame, age_distr: dict) -> pd.Series:
     buildings assigned to be 100 years old.
     :param gdf: GeoDataFrame containing the building data with columns 'height', 'neighbors_count', and 'building_use'
     :param age_distr: dictionary containing the age distribution of the buildings
+    :param residential: boolean value to indicate if the building is residential or not
     :return: Series containng the age of all the buildings.
     """
     # create a series to store the age of the buildings
-    defined_typologies = pd.Series(index=gdf.index, dtype="object")
+    defined_age = pd.Series(index=gdf.index, dtype="object")
+
+    df = pd.DataFrame(columns=["random_numbers"])
+    df["building_usage"] = gdf["building_usage"]
+    df["random_numbers"] = random_assignment(gdf)
+    df["age_code"] = None
+
+    # print(f"check if length are still the same for some reason: {len(df) == len(gdf)}")
+    building_types = ["sfh", "ab", "th", "mfh"]
+    res_mask = df["building_usage"].isin(["sfh", "ab", "th", "mfh"])
+    non_res_mask = ~df["building_usage"].isin(["sfh", "ab", "th", "mfh"])
+
+    # here we are doing only residential buildings for now
+    if residential:
+        # cycle through the residential building types contained in the building_types list
+        for buildings in building_types:
+            cumulative = 0
+            mask_types = df["building_usage"] == buildings
+            mask_ages = age_distr["building_type"] == buildings
+
+            for percentages, id_code in zip(
+                age_distr["percentage"][mask_ages], age_distr["tabula_ID"][mask_ages]
+            ):
+                percentage_mask = (df["random_numbers"] <= percentages) & (
+                    df["building_usage"] == buildings
+                )
+                print(
+                    f"number of {buildings} assigned to category {id_code}: {percentage_mask.sum()}"
+                )
+                cumulative += percentage_mask.sum()
+                print(
+                    f"cumulative amount of {buildings} ({mask_types.sum()}) assigned to category {id_code}: {cumulative}"
+                )
+                df.loc[percentage_mask, "age_code"] = id_code
+
+    defined_age = df["age_code"]
+    return defined_age
+
+
+def random_assignment(gdf: gpd.GeoDataFrame):
+    building_types_list = gdf["building_usage"].unique()
+    random_numbers = pd.Series(index=gdf.index, dtype="object")
+
+    for buildings in building_types_list:
+        mask = gdf["building_usage"] == buildings
+        random_numbers[mask] = np.random.uniform(low=0.0, high=1, size=len(gdf[mask]))
+
+    return random_numbers
+
+
+def refactor_age_percentage(age_distr: pd.DataFrame, dict) -> pd.Series:
 
     return None
 
@@ -135,6 +188,8 @@ if __name__ == "__main__":
     import geopandas as gpd
     import numpy as np
     import sys
+
+    np.random.seed(42)
 
     # insure we can import the qgis_utils module
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -151,26 +206,39 @@ if __name__ == "__main__":
     building_data["building_usage"] = define_building_type(building_data)
     df_data = pd.DataFrame(building_data)
 
+    # create the masks and print the number of buildings of each type
     all_res = df_data["building_usage"].isin(["sfh", "ab", "th", "mfh"])
     mask_sfh = df_data["building_usage"] == "sfh"
     mask_ab = df_data["building_usage"] == "ab"
     mask_th = df_data["building_usage"] == "th"
     mask_mfh = df_data["building_usage"] == "mfh"
 
-    print(
-        "Single Family Homes: ",
-        mask_sfh.sum(),
-        "share:",
-        mask_sfh.sum() / all_res.sum(),
-    )
-    print("Terraced Houses: ", mask_th.sum(), "share:", mask_th.sum() / all_res.sum())
-    print(
-        "Multi Family Homes: ", mask_mfh.sum(), "share:", mask_mfh.sum() / all_res.sum()
-    )
-    print(
-        "Apartment Buildings: ", mask_ab.sum(), "share:", mask_ab.sum() / all_res.sum()
-    )
-    print("all residential buildings", all_res.sum())
+    # print(
+    #     "Single Family Homes: ",
+    #     mask_sfh.sum(),
+    #     "share:",
+    #     mask_sfh.sum() / all_res.sum(),
+    # )
+    # print("Terraced Houses: ", mask_th.sum(), "share:", mask_th.sum() / all_res.sum())
+    # print(
+    #     "Multi Family Homes: ", mask_mfh.sum(), "share:", mask_mfh.sum() / all_res.sum()
+    # )
+    # print(
+    #     "Apartment Buildings: ", mask_ab.sum(), "share:", mask_ab.sum() / all_res.sum()
+    # )
+    # print("all residential buildings", all_res.sum())
+
+    # Age of the building assignment
+
+    path_age = "../Building/data/residential_age.csv"
+    abs_age = os.path.abspath(os.path.join(os.path.dirname(__file__), path_age))
+
+    age_look_up = pd.read_csv(abs_age)
+
+    df_data["age_code"] = define_building_age(df_data, age_look_up, True)
+
+    sfh_mask = df_data["building_usage"] == "sfh"
+    sfh1_mask = df_data[sfh_mask]["age_code"] == 1
 
     # TODO: https://mapview.region-frankfurt.de/maps4.16/resources/apps/RegioMap/index.html?lang=de&vm=2D&s=2543.2619432300075&r=0&c=471481.09638917685%2C5549632.605473744&l=siedlungsflaechentypologie%280%29%2Cstaedtekartetoc%2C-poi_3d%2C-windmills%2C-gebaeude_1
     # this link has a list of all the building types that we can use to define the building types that are not residential.
