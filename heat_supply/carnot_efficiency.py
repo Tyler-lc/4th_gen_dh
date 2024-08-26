41f80d52bff22e4ed2afe9ab77cd6627791863e9
Tyler-lc <lucasamassima@gmail.com>
2024-08-26 18:33:07 +0200

    added module to use carnot efficiency

---
+++ heat_supply/carnot_efficiency.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from typing import Union
+
+
+def carnot_cop(
+    T_hot: pd.Series,
+    T_cold: pd.Series,
+    approach_temperature: float,
+    carnot_efficiency: float = 0.524,
+) -> pd.Series:
+    """
+    Calculate the COP of a heat pump using the Carnot formula.
+
+    Args:
+        T_hot (pd.Series): Hot temperature.
+        T_cold (pd.Series): Cold temperature.
+        approach_temperature (float): Approach temperature to be added.
+        carnot_efficiency (float): Carnot efficiency factor. Default is 0.524.
+
+    Returns:
+        pd.Series: COP values.
+    """
+    COP = pd.DataFrame()
+    T_hot = T_hot + 273.15 + approach_temperature
+    T_cold = T_cold + 273.15 + approach_temperature
+    COP["hot"] = T_hot
+    COP["cold"] = T_cold
+    COP["COP_hourly"] = COP["hot"] / (COP["hot"] - COP["cold"]) * carnot_efficiency
+
+    # Limit the COP to a maximum value of 4
+    COP = COP.clip(upper=4)
+
+    return COP["COP_hourly"]
+
+
+def hp_el_demand(Qhot, COP):
+    """
+    Calculates the electricity demand of a heat pump give the heat demand and the COP.
+    """
+    Qel = Qhot / COP
+    return Qel
