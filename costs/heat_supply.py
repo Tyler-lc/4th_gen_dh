67dc5907ee195eb6be4bd883bf1dd2a164618d71
Tyler-lc <lucasamassima@gmail.com>
2024-08-26 18:32:24 +0200

    changed name and position

--- costs/heat_supply.py
+++ costs/heat_supply.py
@@ -17,7 +17,7 @@ def lorenz_cop(
     The lorentz COP is more accurate than Carnot COP in the case of multistage heat pumps.
     https://ens.dk/sites/ens.dk/files/Analyser/technology_data_catalogue_for_el_and_dh.pdf (pag 288)
     Input temperatures are in °C.
-    The sour
+
     Args:
     t_sink_in: temperature of the sink fluid at the inlet of the heat pump [C]
     t_sink_out: temperature of the sink fluid at the outlet of the heat pump [C]
@@ -56,14 +56,14 @@ def log_temp(t_in, t_out):
 def capital_costs_hp(installed_thermal_capacity: float, source_type: str) -> float:
     """
     Calculate the installation cost of a heat pump based on the heat source type and the installed thermal capacity.
-    The cost is in EUR. Startup costs are ignored.
+    The cost is in Million EUR. Startup costs are ignored.
     Args:
     installed_thermal_capacity: the installed thermal capacity of the heat pump [MW]
     source_type : type of the source fluid. It can be 'air' or 'excess_heat'
     """
     if source_type == "air":
         nominal_investment_total = 1.435 * installed_thermal_capacity ** (-0.219)
-    if source_type == "excess_heat":
+    elif source_type == "excess_heat":
         nominal_investment_total = 1.2858 * installed_thermal_capacity ** (-0.266)
     else:
         raise ValueError("source_type must be 'air' or 'excess_heat'")
@@ -77,8 +77,8 @@ def var_oem_hp(
     thermal_energy_produced: pd.Series,
 ) -> pd.Series:
     """
-    Calculate the variable operation and maintenance (O&M) costs of a heat pump based on the heat source type and the installed thermal capacity.
-    The cost is in EUR/year (assuming we a whole year of thermal_ernergy_produced)
+    Calculate the variable operation and maintenance (O&M) costs of a heat pump based on the heat source type and the amount of Energy produced in the time frame.
+    The cost is in Million EUR/year (assuming we a whole year of thermal_ernergy_produced)
     Args:
     installed_thermal_capacity: the installed thermal capacity of the heat pump [MW]
     source_type : type of the source fluid. It can be 'air' or 'excess_heat'
@@ -88,21 +88,21 @@ def var_oem_hp(
     var_oem_year = pd.Series(index=thermal_energy_produced.index)
 
     if source_type == "air":
-        var_oem = -0.461 * np.log(installed_thermal_capacity) + 2.852
+        var_oem = (-0.461 * np.log(installed_thermal_capacity)) + 2.852
 
-    if source_type == "excess_heat":
-        var_oem = -0.461 * np.log(installed_thermal_capacity) + 2.852
+    elif source_type == "excess_heat":
+        var_oem = (-0.461 * np.log(installed_thermal_capacity)) + 2.852
     else:
         raise ValueError("source_type must be 'air' or 'excess_heat'")
 
     var_oem_year = var_oem * thermal_energy_produced
-    return var_oem_year
+    return var_oem_year.sum() / 1000000
 
 
 def fixed_oem_hp(installed_thermal_capacity: float, source_type: str) -> float:
     """
     Calculate the fixed operation and maintenance (O&M) costs of a heat pump based on the heat source type and the installed thermal capacity.
-    The cost is in EUR/year.
+    The cost is in Million EUR/year.
     Args:
     installed_thermal_capacity: the installed thermal capacity of the heat pump [MW]
     source_type : type of the source fluid. It can be 'air' or 'excess_heat'
@@ -110,19 +110,41 @@ def fixed_oem_hp(installed_thermal_capacity: float, source_type: str) -> float:
     if source_type == "air":
         fixed_oem = 2126.75
 
-    if source_type == "excess_heat":
+    elif source_type == "excess_heat":
         fixed_oem = 2126.75
 
     else:
         raise ValueError("source_type must be 'air' or 'excess_heat'")
 
-    return fixed_oem
+    return fixed_oem / 1000000
 
 
-# TODO: Variable O&M are calculated based on the thermal energy produced by the heat pump \
-# Also they need to have the inflation applied.
-# TODO: the capital costs are calculated based on the installed thermal capacity of the heat pump. Only once.
-# TODO: Fixed O&M are calculated based the thermal installed capacity and are applied every year. We need to apply inflation to them.
+def calculate_lcoh(
+    investment_costs,
+    fixed_om_series,
+    variable_om_series,
+    electricity_costs_series,
+    heat_output_series,
+    discount_rate,
+):
 
-# TODO: create a function to calculate the size of the heat pump based on the thermal energy demand of the buildings in the area.
-# TODO: We might have to set a limit and split the heat pump in two or more heat pumps.
+    years = fixed_om_series.index
+    denominator = 0
+    numerator = 0
+
+    for t in years:
+        discount_factor = (1 + discount_rate) ** t
+        # Sum the discounted costs
+        numerator += (
+            fixed_om_series.iloc[t, 0]
+            + variable_om_series.iloc[t, 0]
+            + electricity_costs_series.iloc[t, 0]
+        ) / discount_factor
+        # Sum the discounted heat output
+        denominator += heat_output_series.iloc[t, 0] / discount_factor
+
+    # Add investment cost as a one-time cost in the first year (not discounted if it's paid upfront)
+    numerator += investment_costs
+
+    lcoh = numerator / denominator
+    return lcoh
