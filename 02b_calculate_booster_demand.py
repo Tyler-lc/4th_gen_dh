77037e7494ea22f8873cfc1bb232a3760fad4ca5
Tyler-lc <lucasamassima@gmail.com>
2024-11-19 11:28:20 +0100

    suppressed efficiency warning from Pandas. Added export of files.

--- 02b_calculate_booster_demand.py
+++ 02b_calculate_booster_demand.py
@@ -7,6 +7,7 @@ import sys
 import shutil
 from multiprocessing import Pool
 from tqdm import tqdm
+import warnings
 
 from building_analysis.Building import Building
 from Person.Person import Person
@@ -14,6 +15,7 @@ from utils.misc import get_mask
 from building_analysis.building_generator import apply_renovations, need_insulation
 from heat_supply.carnot_efficiency import carnot_cop
 
+warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
 
 ########################################################
 # in this file we are going to calculate the energy demand for the booster scenario
@@ -21,13 +23,13 @@ from heat_supply.carnot_efficiency import carnot_cop
 # 50 °C at the inlet and 85 (?) at the outlet. The approach temperature will be 5 °C.
 ########################################################
 
-### safety factor for the heat pump size. This is just an oversizing factor really.
+### safety factor for the heat pump size. This is just an oversizing factor really. It may not be needed here
 safety_factor = 1.2
 
-### we set the supply temperature to the buildings (i.e. outlet temperature)
+### we set the supply temperature to the buildings (i.e. outlet temperature). maybe not needed here
 t_supply = 85  # °C
 
-### and we also set the inlet temperature to the heat pump. Which in this case
+### and we also set the inlet temperature to the booster heat pump. Which in this case
 ### is the temperature coming from the district heating network
 t_grid = 50  # °C
 
@@ -51,7 +53,7 @@ unrenovated_space_heating_path = (
     "building_analysis/results/unrenovated_whole_buildingstock/space_heating"
 )
 
-# Define the paths where the renovated data will be stored
+# Define the paths where the booster data will be stored
 booster_dhw_energy_path = (
     "building_analysis/results/booster_whole_buildingstock/dhw_energy"
 )
@@ -100,8 +102,8 @@ os.makedirs(dir_space_heating, exist_ok=True)
 ### let's start by iterating over the gdf and iterate over the space heating data.
 
 ### First we need to create a new column in the gdf_buildingstock_results to store the heat pump size
-gdf_buildingstock_results["heat_pump_size [kW]"] = 0
-gdf_buildingstock_results["peak_demand_on_dh_grid [kW]"] = 0
+gdf_buildingstock_results["heat_pump_size [kW]"] = 0.0
+gdf_buildingstock_results["peak_demand_on_dh_grid [kW]"] = 0.0
 
 ### we also need to create a new file to store the heat pump electricity demand
 ### along with the folder path
@@ -119,6 +121,7 @@ os.makedirs(booster_dhw_path, exist_ok=True)
 ### First let's retrieve a single space heating file to get the index
 space_heating_path = gdf_buildingstock_results.loc[0, "space_heating_path"]
 space_heating = pd.read_csv(space_heating_path, index_col=0)
+space_heating.index = pd.to_datetime(space_heating.index)
 
 temp_df = pd.DataFrame(
     index=space_heating.index,
@@ -130,11 +133,28 @@ temp_df = pd.DataFrame(
     ],
 )
 
+
 ### we create a temporary dataframe to store the heat pump electricity demand
 ### that we will export to a csv file
+### the carnot_cop method requires a series to work. Hence we need to create a series
+# with the correct length
+index = pd.to_datetime(space_heating.index)
+t_supply_series = pd.Series(t_supply, index=index)
+t_grid_series = pd.Series(t_grid, index=index)
 
-for idx, row in gdf_buildingstock_results.iterrows():
+### we also want to store the results from all of this in some dataframes because
+# we will need it to create the area_results dataframe later on.
 
+area_grid_demand_df = pd.DataFrame(index=index)
+area_space_heating_df = pd.DataFrame(index=index)
+booster_el_demand_df = pd.DataFrame(index=index)
+area_dhw_volume = pd.DataFrame(index=index)
+area_dhw_energy = pd.DataFrame(index=index)
+
+
+for idx, row in tqdm(
+    gdf_buildingstock_results.iterrows(), total=len(gdf_buildingstock_results)
+):
     ### initialize the temp_df with zeros
     temp_df.iloc[:] = 0
 
@@ -145,23 +165,32 @@ for idx, row in gdf_buildingstock_results.iterrows():
     space_heating = pd.read_csv(space_heating_path, index_col=0)
     space_heating.index = pd.to_datetime(space_heating.index)
 
-    ### loading the dhw data
+    ### loading the dhw energy data
     dhw_energy_path = row["dhw_energy_path"]
     dhw_energy = pd.read_csv(dhw_energy_path, index_col=0)
     dhw_energy.index = pd.to_datetime(dhw_energy.index)
 
+    ### loading the dhw volume data
+    dhw_volume_path = row["dhw_volume_path"]
+    dhw_volume = pd.read_csv(dhw_volume_path, index_col=0)
+    dhw_volume.index = index
+
     ### let's sum the dhw and SH demands together to assess total
     ### HP size needed.
-    total_demand = space_heating + dhw_energy
+    total_demand = space_heating.values + dhw_energy.values
     max_demand = total_demand.max()
-    hp_size = max_demand * safety_factor
+    hp_size = float(max_demand * safety_factor)
     gdf_buildingstock_results.loc[idx, "heat_pump_size [kW]"] = hp_size
 
     ### now we need to calculate the COP for these heat pumps
     ### luckily we have the COP function calculator, so we can use that.
     ### we need to iterate over the space heating data and calculate the COP for each hour
-    COP_hourly = carnot_cop(t_supply, t_grid, approach_temperature=5)
-    el_demand = space_heating.values / COP_hourly.values
+    COP_hourly = carnot_cop(t_supply_series, t_grid_series, approach_temperature=5)
+    el_demand = (
+        space_heating["net useful hourly demand [kWh]"].values / COP_hourly.values
+    )
+    el_demand_series = pd.Series(el_demand)
+    el_demand_series.index = index
 
     # original COP eq is COP = Qh / (Qh - Qc)
     # solving for Qc ->
@@ -170,8 +199,10 @@ for idx, row in gdf_buildingstock_results.iterrows():
     # The rest will be provided by the electricity of the booster.
     # because of how EMBERS module works, we need to calculate the maximum
     # value of this demand. This will be then the "demand" in embers.
-    dh_grid_demand = space_heating.values * (1 - 1 / COP_hourly.values)
-    gdf_buildingstock_results.loc[idx, "peak_demand_on_dh_grid [kW]"] = (
+    dh_grid_demand = space_heating["net useful hourly demand [kWh]"] * (
+        1 - 1 / COP_hourly.values
+    )
+    gdf_buildingstock_results.loc[idx, "peak_demand_on_dh_grid [kW]"] = float(
         dh_grid_demand.max()
     )
 
@@ -185,34 +216,49 @@ for idx, row in gdf_buildingstock_results.iterrows():
 
     ### also we need to be able to possibly recover these data later on.
     ### so we will store the path to the csv files in the gdf_buildingstock_results
-    gdf_buildingstock_results.loc[idx, "space_heating_booster_path"] = (
+    gdf_buildingstock_results.loc[idx, "booster_path"] = (
         f"{booster_space_heating_path}/{building_id}_{sim}.csv"
     )
 
-#### the rest of the code needs to be double checked right now.
+    ### before we exit the loop, let's save the data in the dataframe that we will use
+    # later on to save the area_results
 
+    area_grid_demand_df[building_id] = dh_grid_demand
+    area_space_heating_df[building_id] = space_heating
+    booster_el_demand_df[building_id] = el_demand
+    area_dhw_volume[building_id] = dhw_volume.sum(axis=1)
+    area_dhw_energy[building_id] = dhw_energy.sum(axis=1)
 
 # now we can save the results to a file
 gdf_buildingstock_results.to_parquet(
     f"building_analysis/results/{sim}_{size}/buildingstock_{sim}_{size}_results.parquet"
 )
 
-area_results_urenovated_path = (
-    "building_analysis/results/unrenovated_whole_buildingstock/area_results.csv"
+area_results_booster_path = (
+    f"building_analysis/results/{sim}_{size}/area_results_{sim}.csv"
 )
 
-# area_results = pd.DataFrame(
-#     0,
-#     index=inside_temp.index,
-#     columns=["dhw_volume", "dhw_energy", "space_heating"],
-# )
-
-area_results_unrenovated = pd.read_csv(area_results_urenovated_path, index_col=0)
-area_results_unrenovated.index = pd.to_datetime(area_results_unrenovated.index)
-
-area_results["dhw_energy"] = area_results_unrenovated["dhw_energy"]
-area_results["dhw_volume"] = area_results_unrenovated["dhw_volume"]
-
-area_results.to_csv(
-    "building_analysis/results/renovated_whole_buildingstock/area_results_renovated.csv"
+area_results = pd.DataFrame(
+    columns=[
+        "area grid demand [kWh]",
+        "area total boosters demand [kWh]",
+        "area space heating demand [kWh]",
+        "area dhw energy demand [kWh]",
+        "area dhw volume demand [l]",
+    ],
+    index=index,
 )
+area_results["area grid demand [kWh]"] = area_grid_demand_df.sum(axis=1)
+area_results["area total boosters demand [kWh]"] = booster_el_demand_df.sum(axis=1)
+area_results["area space heating demand [kWh]"] = area_space_heating_df.sum(axis=1)
+area_results["area dhw energy demand [kWh]"] = area_dhw_energy.sum(axis=1)
+area_results["area dhw volume demand [l]"] = area_dhw_volume.sum(axis=1)
+
+folder_area_results_path = f"building_analysis/results/{sim}_{size}/area_results"
+os.makedirs(folder_area_results_path, exist_ok=True)
+
+area_results_booster_path = (
+    f"building_analysis/results/{sim}_{size}/area_results/area_results_{sim}_{size}.csv"
+)
+
+area_results.to_csv(area_results_booster_path)
